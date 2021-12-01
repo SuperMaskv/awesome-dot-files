5d38fd4f6c96520b0d0d7f203a37354546b9feb7
laiiihz <laiiihz@outlook.com>
2021-12-01 16:26:56 +0800

    add flutter initial script

--- README.md
+++ README.md
@@ -3,7 +3,9 @@
 收集各类开发工具的配置文件，文件内包含配置好的常用国内镜像等内容
 
 * maven
-* flutter(包含dart)
+* flutter(including dart)
+  * macos initial script
+    > tested on macOS 12.0.1 with zsh
 
 ## Roadmap
 
---
+++ flutter/macos_init.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+FLUTTER_VERSION="2.5.3"
+FLUTTER_CHANNEL="stable"
+DOWNLOAD_PATH="https://storage.flutter-io.cn/flutter_infra_release/releases/$FLUTTER_CHANNEL/macos/flutter_macos_$FLUTTER_VERSION-$FLUTTER_CHANNEL.zip"
+SDK_DOWNLOADED_PATH="$HOME/Downloads/flutter_sdk.zip"
+echo "download flutter sdk to $SDK_DOWNLOADED_PATH"
+curl -o $SDK_DOWNLOADED_PATH $DOWNLOAD_PATH
+RAW_PATH=$HOME/Development
+mkdir $RAW_PATH
+unzip $SDK_DOWNLOADED_PATH -d $RAW_PATH
+rm $SDK_DOWNLOADED_PATH
+
+echo "setting up env"
+echo "export PUB_HOSTED_URL=\"https://pub.flutter-io.cn\"">>$HOME/.zprofile
+echo "export FLUTTER_STORAGE_BASE_URL=\"https://storage.flutter-io.cn\"">>$HOME/.zprofile
+echo "export PATH=\"\$PATH:$RAW_PATH/flutter/bin\"">>$HOME/.zprofile
+echo "export PATH=\"\$PATH:$RAW_PATH/flutter/.pub-cache/bin\"">>$HOME/.zprofile
+echo "export PATH=\"\$PATH:$RAW_PATH/flutter/bin/cache/dart-sdk/bin\"">>$HOME/.zprofile
+
+source $HOME/.zprofile
+flutter doctor -v
+flutter precache
+flutter --version
+
+
+
+
